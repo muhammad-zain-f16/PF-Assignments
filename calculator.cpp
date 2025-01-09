2d2f0be7dc2caf25f2e6e2b963a177301c8b3581
zain <i242609@isb.nu.edu.pk>
2025-01-09 08:46:43 +0500

    Perform basic calculation on int and return int as a result

--- calculator.cpp
+++ calculator.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int char_to_int(char x);
 int number(char [], int &sindex); // this function will make numbers from the evaluate string
+int mult_divide(char [], int &index);
 
 int main()
 {
@@ -11,7 +12,25 @@ int main()
     char eval_string[500] ;
     cout << "Enter expression to evaluate: ";
     cin >> eval_string;
-
+    
+    int ans = 0; // Final answer will be stored here
+    for (int i =0;  eval_string[i] != '\0';)
+    {
+      if (eval_string[i] == '-')
+      {
+        ans -= mult_divide(eval_string, ++i);
+      }
+      else if (eval_string[i] == '+')
+      {
+        ans += mult_divide(eval_string, ++i);
+      }
+      else 
+      {
+        ans += mult_divide(eval_string, i);
+      }
+    
+    }
+    cout << "Answer = " << ans <<endl;
 
     return 0;
 }
@@ -32,4 +51,22 @@ int number( char str[], int &index)
     index++;
   }
   return n;
+}
+
+// This function will perform the calculation for multiplication and divsion and return the answer
+int mult_divide(char num[], int &index)
+{
+    int temp = number(num, index); // this will hold the first the number
+    while (num[index] != '\0' && num[index] != '+' && num[index] != '-')
+    {
+      if ( num[index] == '*')
+      {
+        temp *= number (num, ++index);
+      }
+      else if (num[index] == '/')
+      {
+        temp /= number (num, ++index);
+      }
+    }
+  return temp;
 }

a423a3234b5defa383d1eca916bf852d9c15a15a
zain <i242609@isb.nu.edu.pk>
2025-01-09 08:15:48 +0500

    User input.
    Ask user to input an expression to evaluate which would be string stored the string in character array.

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+    // user input expression 
+    char eval_string[500] ;
+    cout << "Enter expression to evaluate: ";
+    cin >> eval_string;
+
+
+    return 0;
+}

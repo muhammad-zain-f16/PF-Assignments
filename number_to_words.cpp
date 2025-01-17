8f7f8665bef5f2c80d9f36cd1569a0a8d6a34ca1
Muhammad Zain <i242609@isb.nu.edu.pk>
2025-01-17 12:17:10 +0500

    number to words using conditionals and loops

--- number_to_words.cpp
+++ number_to_words.cpp
@@ -1,117 +1,215 @@
 #include <iostream>
 using namespace std;
 
-// 1 to 19 2d array
-const char one_to_nineteen[][12] = 
-{
-  "","One ", "Two ", "Three ", "Four ", "Five ", "Six ",
-  "Seven ", "Eight ", "Nine ", "Ten ", "Eleven ", "Twelve ",
-  "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ",
-  "Eighteen ", "Nineteen "
-};
+// Integer to English words 123 -> One Hundred Twenty Three
+// 1 -> One
+// 10 -> Ten
+// 11 -> Eleven
+// 0 -> Zero
+// less than 20
+// 21 -> Twenty One 21 % 10 = 1 20 % 10 = 0 Twenty
+const char one_to_nineteen[][12] =
+    {
+        "", "One", "Two", "Three", "Four", "Five", "Six",
+        "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+        "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+        "Eighteen", "Nineteen"};
 
-const char teen_num[][10] = 
-{
-  "","","Twenty ", "Thirty ","Fourty ",
-  "Fifty ", "Sixty ", "Seventy ","Eighty ",
-  "Ninety "
-};
-
-
-
+// less than 100
+// 21 / 10 -> 2
+const char teen_num[][10] =
+    {
+        "", "", "Twenty", "Thirty", "Fourty",
+        "Fifty", "Sixty", "Seventy", "Eighty",
+        "Ninety"};
 
 // Functions prototype
-int input_num(string input);
+int input_num(char[]);
 
-void num_to_words(int);
+void num_to_words(long long int);
 
 void show_one_19(int num);
 
 int main()
 {
-  string user_input;
+  char user_input[20];
   cout << "Enter a number that you want to write in words: ";
-  getline(cin, user_input);
-  
-  int num = input_num(user_input);
-  //Check user input
-  while (num <= 0)
+  cin.getline(user_input, 20);
+
+  // convert string to integer
+  long long int num = input_num(user_input);
+
+  // edge case
+  if (num == 0)
   {
-    cout << "Please enter a cheque amount that is bigger than zero: ";
-    getline(cin, user_input);
-    num = input_num(user_input);
+    cout << "Number to words = Zero" << endl;
+    return 0;
   }
 
+  // Check user input
+  while (num < 0)
+  {
+    cout << "Please enter a cheque amount that is bigger than zero: ";
+    cin.getline(user_input, 20);
+    num = input_num(user_input);
+  }
+cout << num << endl;
+  // print the result
   cout << "Number to words = ";
-  num_to_words(num); 
-  
+  num_to_words(num);
+
   return 0;
 }
 
-int input_num(string a)
+int input_num(char arr[]) // arr[] = "12,486" - > 12486
 {
-  int input = 0;
-  int position = 10;
-  bool neg = false;
-    
-  for (int i = 0; a[i] != '\0'; i++)
+  long long int input = 0;     // input -> 12
+  int position = 10; // 1 12 - >
+  bool neg = false;  // if the number is negative or not
+
+  for (int i = 0; arr[i] != '\0'; i++)
   {
     if (neg)
     {
-      if (a[i] >= '0' && a[i] <= '9')
+      if (arr[i] >= '0' && arr[i] <= '9')
       {
         input *= position;
-        input -= a[i] - '0';
+        input -= arr[i] - '0'; // 0-1 = -1* 10 = -10 -2 = -12 * 10 = -123
       }
     }
-    else if (a[i] >= '0' && a[i] <= '9')
+    else if (arr[i] >= '0' && arr[i] <= '9') // Ascii value of arr[i] >= 48 && arr[i] <= 57
     {
-      input *= position;
-      input += a[i] - '0';
+      input *= position;     // input -> 12 * 10 = 120
+      input += arr[i] - '0'; // 51 - 48 = 120 + 3 = 123
     }
-    else if (a[i] == '-')
+    else if (arr[i] == '-')
       neg = true;
-      
   }
   return input;
 }
-void num_to_words(int num)
+
+void num_to_words(long long int num)
 {
-  int index = 0;
+  string a;
+  if (num >= 1000000000)
+  {
+    a += one_to_nineteen[num / 1000000000];
+    a += " Billion";
+    num = num % 1000000000;
+    if (num > 0)
+    {
+      a += " ";
+    }
+  }
+  if (num >= 1000000)
+  {
+    if ((num / 1000000) % 1000 >=100)
+    {
+      a += one_to_nineteen[num / 100000000];
+      a += " Hundred";
+      if ((num / 1000000) % 100 >= 20)
+      {
+      if ((num / 1000000) % 100 != 0)
+      {
+        a+= " ";
+        a += teen_num[(num / 10000000) % 10];
+      }
+      }
+      
+      if ((num/ 1000000)% 10 != 0)
+      {
+        a += " ";
+        a += ((num/ 1000000)% 100 < 20 ? one_to_nineteen[(num / 1000000) % 100]: one_to_nineteen[(num / 1000000) % 10]);
+        
+      }
+      
+    }
+    else if ((num / 1000000) >= 20)
+    {
+      a += teen_num[num / 10000000];
+      if ((num/ 1000000)% 10 != 0)
+      {
+        a += " ";
+        a += one_to_nineteen[(num / 1000000) % 10];
+      }
+    }
+    else
+    {
+      a += one_to_nineteen[num / 1000000];
+    }
+    a += " Million";
+    num = num % 1000000;
+    if (num > 0)
+    {
+      a += " ";
+    }
+  }
+
+  if (num >= 100000)
+  {
+    a += one_to_nineteen[num / 100000];
+    a += " Hundred";
+    num = num % 100000;
+    if (num < 1000)
+    {
+      a += " Thousand";
+      if (num > 0)
+      {
+        a += " ";
+      }
+    }
+    else
+    {
+      a += " ";
+    }
+    
+  }
   if (num >= 1000)
   {
-    index = num / 1000;
-    show_one_19(index);
-    cout << "Thousand ";
+    if (num / 1000 >= 20)
+    {
+      a += teen_num[num / 10000];
+      if ((num / 1000) % 10 != 0)
+      {
+        a += " ";
+        a += (one_to_nineteen[(num / 1000) % 10]);
+      }
+    }
+    else
+    {
+      a += one_to_nineteen[num / 1000];
+    }
+    a += " Thousand";
     num = num % 1000;
-
+    if (num > 0)
+    {
+      a += " ";
+    }
   }
+
   if (num >= 100)
   {
-    index = num / 100;
-    show_one_19(index);
-    cout << "Hundred ";
+    a += one_to_nineteen[num / 100];
+    a += " Hundred";
     num = num % 100;
-
+    if (num > 0)
+    {
+      a += " ";
+    }
   }
-  if (num >= 20)
+
+  if (num >= 20) // twenty three "twenty" and then three 23/ 10 = 2 23 % 10 = 3; num % 10 != 0;
   {
-    index = num / 10;
-    cout << teen_num[index];
+    a += teen_num[num / 10];
     num = num % 10;
+    if (num > 0)
+    {
+      a +=  " ";
+    }
   }
-  if (num > 0)
+  if (num < 20)
   {
-    show_one_19(num);
+    a += one_to_nineteen[num];
   }
-  
-  cout << endl;
-}
-
-void show_one_19(int num)
-{
-  if (num > 0)
-  {
-    cout << one_to_nineteen[num];
-  }
+  cout << a <<  endl;
 }
